5b88dcbf4e3d88a3ad76f050ec0df06331a8e0ca
Artūras Mickevičius <arturas.mickevicius@stud.techin.lt>
2025-01-13 16:34:08 +0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,1 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import js from "@eslint/js";
-// import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  // eslintPluginPrettierRecommended,
-  pluginJs.configs.recommended,
-  js.configs.recommended,
-  {
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-    },
-  },
-];
