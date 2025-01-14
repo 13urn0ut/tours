e5d0dae5271937a2a0f4cf4e335e0269c700fd99
13urn0ut <arturas.mickevicius@stud.techin.lt>
2025-01-14 09:36:33 +0400

    Change getAllTours model

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,8 +2,12 @@ const { sql } = require("./../dbConnection");
 
 exports.getAllTours = async () => {
   const tourList = await sql`
-    SELECT tours.*
-    FROM tours;
+    SELECT tours.name as name, tours.price as price, categories.name as category, difficulties.level as difficulty
+    FROM tours
+    JOIN difficulties
+    ON tours.difficulty_id = difficulties.id
+    JOIN categories
+    ON tours.category_id = categories.id;
     `;
 
   return tourList;
